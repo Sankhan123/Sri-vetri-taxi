a9b49ab9a9add3df06ee335155da41050d8c3b77
Sharan AJ <sharanajs2001@gmail.com>
2021-12-20 10:51:13 +0530

    warnings are rectified

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,11 +5,11 @@ function Profile() {
     return (
 
 
-        <div class="col-sm-9 my-auto col-md-7 mt-5 col-lg-5 mx-auto">
-            <div class="card border-0 shadow pt-4 mt-5 rounded-3 ">
-                <div class=" p-4 p-sm-5">
+        <div className="col-sm-9 my-auto col-md-7 mt-5 col-lg-5 mx-auto">
+            <div className="card border-0 shadow pt-4 mt-5 rounded-3 ">
+                <div className=" p-4 p-sm-5">
                     <Logo/>
-                    <div class="d-grid mt-5">
+                    <div className="d-grid mt-5">
                         <Link to="/one-day-trip" className='border border-2 border-warning mt-4 fw-bold  text-dark  btn btn-outline-warning'>One Day Trip</Link>
                         <Link to="/taxi-trip" className='border border-2 border-warning mt-3 fw-bold  text-dark  btn btn-outline-warning'>Normal Taxi</Link>
                         <Link to="/local-trip" className='border border-2 border-warning mt-3 fw-bold  text-dark  btn btn-outline-warning'>Local Trip</Link>
